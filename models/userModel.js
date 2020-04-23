5642a550da9bb28617bb8bf4eb38ffafc3ce6edb
Garrett Roberts <garrettmroberts@gmail.com>
2020-04-23 08:33:38 -0500

    added user model

--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,29 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-
+  email: {
+    type: String,
+    match: /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true,
+    match: /[a-zA-Z0-9]{3,}/
+  },
+  firstName: {
+    type: String,
+    lowercase: true,
+    required: true
+  },
+  lastName: {
+    type: String,
+    lowercae: true,
+    required: true
+  },
+  goals: Array,
+  equipment: Array
 })
 
 const User = mongoose.model("User", userSchema);
