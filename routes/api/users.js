5f2dbd00e6684a3784a7149d4e79cd076fd99ffb
Garrett Roberts <garrettmroberts@gmail.com>
2020-04-23 17:49:01 -0500

    added user apis

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,3 +1,19 @@
 const router = require("express").Router();
+const userController = require("../../controllers/userController");
+
+// Matches with /api/users
+router
+  .route("/")
+  .get(userController.findAll)
+  .post(userController.addUser)
+  .put(userController.updateUser)
+  .delete(userController.deleteUser);
+
+// Matches with /api/users/:id
+router
+  .route("/:id")
+  .get(userController.findByID);
+
+
 
 module.exports = router;
