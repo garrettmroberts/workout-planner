4f004afef1a51016549a039dd16f70fe0fb47787
Garrett Roberts <garrettmroberts@gmail.com>
2020-04-23 11:32:37 -0500

    added preliminary passport support

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("./scripts/passport");
 const PORT = process.env.PORT || 3001;
 const routes = require("./routes");
 const app = express();
 
+// Init app middleware
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+// Connects setver to routes
+app.use(routes);
+
+// Enables passport verification
+app.use(session({ secret: 'apple butter', resave: true, saveUninitialize: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connects to mongoDB
 const mongooseOptions = {
   useUnifiedTopology: true,
   useNewUrlParser: true
 };
-
-app.use(routes);
-
 mongoose.connect((process.env.MONGODB_URI || "mongodb://localhost/workoutPlannerDB"), mongooseOptions);
 
+// Sets server to listen at PORT
 app.listen(PORT, () => console.log(`API server listening at http://localhost:${PORT}`));
