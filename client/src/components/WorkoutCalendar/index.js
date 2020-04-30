b008b5d23ac9b15cefa662b1d32b1ead448639fd
Garrett Roberts <garrettmroberts@gmail.com>
2020-04-30 15:03:48 -0500

    calendar populates on ui

--- client/src/components/WorkoutCalendar/index.js
+++ client/src/components/WorkoutCalendar/index.js
@@ -2,10 +2,19 @@ import React from "react";
 import axios from "axios";
 import { useStoreContext, StoreProvider } from "../../utils/GlobalState";
 import Card from "../Card";
+import "./style.css";
 
 function WorkoutCalendar() {
+  const [state] = useStoreContext();
+  console.log(state.currentUser.calendar);
+  const workoutCards = state.currentUser.calendar.map(({ day, workouts }) => {
+    return <Card day={day} key={day} workouts={workouts}/>
+  })
+
   return(
-    <Card />
+    <div className="card-holder">
+      {workoutCards}
+    </div>
   );
 };
 
