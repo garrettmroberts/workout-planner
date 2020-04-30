08e73cffb5a10898f80e800118cfffa98f89f86c
Garrett Roberts <garrettmroberts@gmail.com>
2020-04-30 09:39:08 -0500

    form submission queries for workouts that match desired equipment

--- client/src/pages/Calendar.js
+++ client/src/pages/Calendar.js
@@ -5,15 +5,9 @@ import WorkoutCalendar from "../components/WorkoutCalendar";
 import API from '../utils/API';
 import "./calendar.css";
 
-function useForceUpdate() {
-  const [value, setValue] = useState(0); // integer state
-  return () => setValue(value => ++value); // update the state to force render
-}
-
 function Calendar() {
   const [store] = useStoreContext();
 
-  const forceUpdate = useForceUpdate();
   const renderComponents = () => {
     if (store.currentUser.goal) {
       return <WorkoutCalendar />
@@ -21,6 +15,7 @@ function Calendar() {
       return <GoalsForm />
     };
   };
+  
   return(
     <div className="wrapper">
       {renderComponents()}
