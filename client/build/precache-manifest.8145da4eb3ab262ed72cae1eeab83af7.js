48722a58851a7646cea6636d085ff10b0bd8efab
chizz <cbrittingham@gmail.com>
2020-05-04 07:07:52 -0500

    ran build

---
+++ client/build/precache-manifest.8145da4eb3ab262ed72cae1eeab83af7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4579bfa6e19163ba819d8a4cc4ece09f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3d538e6a5df6a7e188a",
+    "url": "/static/css/main.4d856995.chunk.css"
+  },
+  {
+    "revision": "f58b38efa8b288bbedce",
+    "url": "/static/js/2.ef8c0445.chunk.js"
+  },
+  {
+    "revision": "6e0dc15b9d76a3f1c0f79d7998ffa159",
+    "url": "/static/js/2.ef8c0445.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3d538e6a5df6a7e188a",
+    "url": "/static/js/main.b25e8ccc.chunk.js"
+  },
+  {
+    "revision": "2a0ece828d3e3394dfc3",
+    "url": "/static/js/runtime-main.34d64dfa.js"
+  }
+]);
